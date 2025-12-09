bd7b20a8d21fc714fec307bd53c926b070b6656c
loup <sebastian@nieciecki.pl>
2025-12-10 00:14:48 +0100

    refactor(read): print metadata of 3mf file in table format

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
-def main():
-    print("Hello from inventor3d!")
+import lib3mf
+import tabulate
 
+wrapper = lib3mf.get_wrapper()
 
-if __name__ == "__main__":
-    main()
+model = wrapper.CreateModel()
+
+if model is not None and isinstance(model, lib3mf.Model):
+    reader = model.QueryReader(ReaderClass="3mf")
+    reader.ReadFromFile("resources/example.3mf")
+    meta = model.GetMetaDataGroup()
+    meta_table = [["name", "key", "type", "value"]]
+
+    if isinstance(meta, lib3mf.MetaDataGroup):
+        for index in range(meta.GetMetaDataCount()):
+            data = meta.GetMetaData(index)
+            name = data.GetName()
+            key = data.GetKey()
+            type = data.GetType()
+            value = data.GetValue()
+            meta_table.append([name, key, type, value])
+
+        print(tabulate.tabulate(meta_table, headers="firstrow"))
